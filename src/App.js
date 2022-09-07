859e0a6cab5df56014383ff7ffb1b4b6c7377871
danbg <danbgavindv90835gmail.com>
2022-09-07 16:33:13 +0600

    create ico and Title v0.2.4

--- src/App.js
+++ src/App.js
@@ -17,11 +17,19 @@ import PoorhousePage from './pages/poorhousePage/PoorhousePage';
 import MercyBusPage from './pages/mercyBusPage/MersyBusPage';
 import SundaySchoolPage from './pages/sundaySchoolPage/SundaySchoolPage';
 import PhotogalleryPage from './pages/photogalleryPage/PhotogalleryPage';
-
+import {React, useEffect} from 'react'
+import icon from './images/favicon.ico';
 import { BelltowerGallery, MersybusGallery, PoorhouserGallery, TempleGallery } from './pages/photogallerypages/PhotogalleryPages';
 import { DonationsPage } from './pages/donationsPage/DonationsPage';
+
 function App() {
+  useEffect(() => {
+    const favicon = document.getElementById('favicon');
+    favicon.setAttribute('href', icon);
+}, []);
   return (
+
+    
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<MainPage />}/>
